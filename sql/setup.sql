aba4378582f13418af5581eb2f5d6d5ebc9aa291
Pedro <pedro.chn01@gmail.com>
2022-08-01 18:06:10 -0700

    starting

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,15 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+
+CREATE TABLE books (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    title VARCHAR,
+    released BIGINT
+);
+
+CREATE TABLE author (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name VARCHAR,
+    dob 
+    pob VARCHAR
+);
