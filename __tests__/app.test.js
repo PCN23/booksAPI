c30581aa74fdad3159a889cd795d143fadb44586
Pedro <pedro.chn01@gmail.com>
2022-08-02 17:41:21 -0700

    passing first test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,18 +1,33 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-const { request } = require('express');
-// const request = require('supertest');
-// const app = require('../lib/app');
+//const { request } = require('express');
+const request = require('supertest');
+const app = require('../lib/app');
 
 describe('books route', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('should return a list of books', async () => {
+  it('should return a book', async () => {
     const res = await request(app).get('/books');
-    expect(res.body.length).toEqual(1);
+    expect(res.body.length).toEqual(6);
+    console.log(res);
   });
+
+  // it('/books/:id should return book detail', async () => {
+  //   const res = await request(app).get('/books/1');
+  //   console.log(res.body);
+  //   const principles = {
+  //     id: '1',
+  //     name: 'Principles',
+  //     released: 2009
+  //   };
+  //   expect(res.body).toEqual(principles);
+  // });
+
   afterAll(() => {
     pool.end();
   });
 });
+
+
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,5 +1,6 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS books;
 
 CREATE TABLE books (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -7,9 +8,22 @@ CREATE TABLE books (
     released BIGINT
 );
 
-CREATE TABLE author (
-    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    name VARCHAR,
-    dob DATE,
-    pob VARCHAR
-);
+-- CREATE TABLE author (
+--     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+--     name VARCHAR,
+--     dob DATE,
+--     pob VARCHAR
+-- );
+
+INSERT INTO books (
+    title,
+    released
+)
+VALUES
+('Principles', '2009'),
+('How to roof a house', '1995'),
+('How to make a living', '1999'),
+('Hoop like James', '2003'),
+('Legends are never gone', '2012'),
+('Soccer', '2003')
+;
